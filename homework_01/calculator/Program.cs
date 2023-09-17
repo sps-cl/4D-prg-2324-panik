616cac5a1bbaee132f0de93e21ec348d42d016af
JanusPanikus <janpanikcz@post.cz>
2023-09-17 16:43:28 +0200

    dokončení projektu homework_01

--- homework_01/calculator/Program.cs
+++ homework_01/calculator/Program.cs
@@ -1,2 +1,116 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// abstraktni trida IOperation (klicove slovo interface)
+// nemuzeme k ni pristupovat primo ale pouze pred jeji potomky (child : parent) 
+public interface IOperation
+{
+    int Execute(int a, int b);
+}
+
+// trida OpAdd - potomek tridy IOperation
+public class OpAdd : IOperation
+{
+    public int Execute(int a, int b)
+    {
+        return a + b;
+    }
+}
+
+// trida OpSub - potomek tridy IOperation
+public class OpSub : IOperation
+{
+    public int Execute(int a, int b)
+    {
+        return a - b;
+    }
+}
+
+// trida OpMagic - potomek tridy IOperation
+public class OpMagic : IOperation
+{
+    // deklarace Random uvnitr tridy OpMagic (private access modifier)
+    private Random random;
+
+    public OpMagic(Random random)
+    {
+        this.random = random;
+    }
+
+    public int Execute(int a, int b)
+    {
+        // generovani nahodneho integeru pro magickou operaci
+        int randomNumber = random.Next(0, 3);
+
+        // vyber magicke operace
+        switch (randomNumber)
+        {
+            case 0:
+                return a + b;
+            case 1:
+                return a - b;
+            case 2:
+                return a * b;
+            default:
+                // po konzultaci s ChatGPT
+                throw new InvalidOperationException("Invalid random number.");
+        }
+    }
+}
+
+
+public class CalculatorContext
+{
+    // deklarace operation typu IOperation
+    public IOperation operation;
+
+    public CalculatorContext(IOperation operation)
+    {
+        this.operation = operation;
+    }
+
+    public int ExecuteOperation(int a, int b)
+    {
+        return operation.Execute(a, b);
+    }
+}
+
+
+// rozhrani CalculatorApp
+public class CalculatorApp
+{
+    static void Main()
+    {
+        Console.WriteLine("Zadej první operand:");
+        int a = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zadej druhý operand:");
+        int b = int.Parse(Console.ReadLine());
+        Console.WriteLine("Zadej operaci (+, -, ?):");
+        string selectedOperation = Console.ReadLine();
+
+        IOperation op;
+
+        if (selectedOperation == "+")
+        {
+            op = new OpAdd();
+        }
+        else if (selectedOperation == "-")
+        {
+            op = new OpSub();
+        }
+        else if (selectedOperation == "?")
+        {
+            Random random = new Random();
+            op = new OpMagic(random);
+        }
+        else
+        {
+            Console.WriteLine("Zkus jinou operaci blude");
+            return;
+        }
+        
+        // novy objekt calculator tridy CalculatorContext
+        CalculatorContext calculator = new CalculatorContext(op);
+        Console.Write($"Výsledek operace {calculator.operation} ({a} {selectedOperation} {b}) je: {calculator.ExecuteOperation(a, b)}");
+        Console.ReadLine();
+    }
+}
